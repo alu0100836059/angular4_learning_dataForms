2bfb566d3a7df8c1bea7817ba33b93fb732ecc1b
alu0100836059 <alu0100836059@ull.edu.es>
2017-10-23 23:30:06 +0200

    Working with forms (template driven forms)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  onSubmit = function(user) {
+    console.log(user);
+  }
 }
